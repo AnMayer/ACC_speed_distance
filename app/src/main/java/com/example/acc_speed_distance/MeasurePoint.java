184bebe1a8b6896f570253a484a7f4dc0e1c7e45
MrBlackDog <sasha29091999@yandex.ru>
2020-05-30 11:58:38 +0300

    added comment to the calc method. Need to change formulas in this method.

--- app/src/main/java/com/example/acc_speed_distance/MeasurePoint.java
+++ app/src/main/java/com/example/acc_speed_distance/MeasurePoint.java
@@ -7,7 +7,7 @@ public class MeasurePoint {
     private float speedBefore;
     private float speedAfter;
     private long interval;
-
+    private float distance;
 
     public MeasurePoint(float x, float y, float z, float speedBefore, long interval) {
         this.x = x;
@@ -18,17 +18,24 @@ public class MeasurePoint {
         speedAfter = 0;
         calc();
     }
-public float getSpeedAfter()
-{
-    return speedAfter;
-}
+    public float getSpeedAfter()
+    {
+         return speedAfter;
+    }
 
+    public float getDistanceAfter()
+    {
+        return distance;
+    }
+
+    /**формулы - полная хуйня для равноускоренного движения,
+     * что вообще не совместимо с жизнью, надо численно интегрировать каждое измерение
+     */
     private void calc(){
         //Acceleration as projection of current vector on average
         float acceleration = (float) Math.sqrt(this.x * this.x + this.y * this.y * +this.z * this.z);
         float t = ((float)interval / 1000f);
         speedAfter = speedBefore + acceleration * t;
-        float distance = speedBefore * t + acceleration * t * t / 2;
-
+        distance = speedBefore * t + acceleration * t * t / 2;
     }
 }
